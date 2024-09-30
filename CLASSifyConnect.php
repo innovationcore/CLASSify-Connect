501508e240df74ff8d9ad85ad5e0be3541e546d7
Noah Perry <ncpe227@uky.edu>
2024-09-30 16:36:46 +0000

    working on config

--- CLASSifyConnect.php
+++ CLASSifyConnect.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace ExternalModuleExercises\HelloWorldModule;
+namespace CAAIModules\CLASSifyConnect;
 
 use ExternalModules\AbstractExternalModule;
 
-class HelloWorldModule extends AbstractExternalModule {
+class CLASSifyConnect extends AbstractExternalModule {
 
     // This is generally where your module's hooks will live
     function redcap_every_page_top($project_id) {
         print_r('Hello world! I am a message produced by a hook!');
     }
 
+    
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 This project is a REDCap module intended to allow you to publish your forms' data to CLASSify and automatically generate a report from a variety of machine learning models without needing to download data manually or interface with an external application.
+
+Once you have placed the package in the modules folder, make sure to add a _v0.0.0 to the end, or it will not work. This will eventually be replaced with a more automatic versioning system.
