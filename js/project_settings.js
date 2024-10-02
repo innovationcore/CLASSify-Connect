3abc2648d52629a13564dd434cee5336b8ac936b
Noah Perry <ncpe227@uky.edu>
2024-10-02 16:12:02 +0000

    updated the response of email check to display text instead of an alert.

--- js/project_settings.js
+++ js/project_settings.js
@@ -7,14 +7,15 @@ function checkEmail(email) {
     $.get(`https://data.ai.uky.edu/classify/users/getUserFromEmail?email=${email}`, function(data, status) {
         let response = JSON.stringify(data);
         response = JSON.parse(response)
+        const res_element = document.getElementById('response');
+        console.log(res_element);
+
         if (!response.success) {
-            if (window.confirm(`${email} is not registered with CLASSify. Press OK to be directed to the Center for Applied AI Collaboration Form which allows you to apply for access. 
-            \n\nNote: Your browser may block this popup. Allow it access to be redirected to the form.`)) {
-               window.open('https://redcap.uky.edu/redcap/surveys/?s=K7WTCDH37AXLEKNM', '_blank');
-            }
+            res_element.innerText = `${email} is not registered with CLASSify. Fill out the <a href='https://redcap.uky.edu/redcap/surveys/?s=K7WTCDH37AXLEKNM' target='_blank'>Center for Applied AI Collaboration Form</a> which allows you to apply for access. 
+            \n\nNote: Your browser may block this popup. Allow it access to be redirected to the form.`;
         }
         else {
-            alert(`${email} is registered with CLASSify. You may proceed.`);
+            res_element.innerText = `${email} is registered with CLASSify. You may proceed.`;
         }
     })
 }
